496429f5f1d45691bfd6fc0ed5ccb98ca6ac2589
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-01-17 11:30:16 +0100

    class and json in one place

--- databaseTest/database.py
+++ databaseTest/database.py
@@ -23,3 +23,18 @@ person_string = '{"name": "Bob", "languages": "English", "numbers": [2, 1.6, nul
 person_dict = json.loads(person_string)
 # Pretty Printing JSON string back
 print(json.dumps(person_dict, indent = 4, sort_keys=True))
+
+
+#I think I know classes! Don't judge naming. It is just to better understand it.
+class test:
+  def __init__(gay, hi, fuck):
+    gay.hello = hi
+    gay.sex = fuck
+    
+  def lol(gay):
+    print("Hi, I'm " + str(gay.sex) + " and I want to intercourse " + gay.hello)
+
+text = test("You", 18)
+print(text.hello)
+print(text.sex)
+text.lol()
