cf5197a9d53b7398d4c8ce25349878dbdd95e011
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-01-17 08:42:56 +0100

    -

--- databaseTest/database.py
+++ databaseTest/database.py
@@ -2,7 +2,6 @@ print("\033c")
 import json
 
 data='{"t":"te"}'
-
 try:
     with open("data.json", 'r', encoding='utf-8') as json_config:
         lang = json.load(json_config)
@@ -15,18 +14,12 @@ except:
     
 # Getting dictionary
 person_dict = json.loads(data)
-
 # Pretty Printing JSON string back
 print(json.dumps(person_dict, indent = 4, sort_keys=True))
 
-
-import json
 
 person_string = '{"name": "Bob", "languages": "English", "numbers": [2, 1.6, null]}'
-
 # Getting dictionary
 person_dict = json.loads(person_string)
-
 # Pretty Printing JSON string back
 print(json.dumps(person_dict, indent = 4, sort_keys=True))
-
